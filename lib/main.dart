7dffa9d036addc4610a6a9cc4ff71880b0258d42
Felix Angelov <felangelov@gmail.com>
2025-10-01 20:43:01 -0500

    refactor: add `runDartFrogServer`

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:shelf_web_server/shelf_web_server.dart';
+import 'package:path/path.dart' as p;
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,12 +18,28 @@ class MyApp extends StatelessWidget {
           child: Column(
             children: [
               Text('Welcome to Dart Frog Desktop!'),
-              ElevatedButton(onPressed: () {}, child: const Text('Start Server with Dart Frog Web Server')),
-              ElevatedButton(onPressed: () => runShelfServer(), child: const Text('Start Server with Shelf Web Server')),
+              ElevatedButton(
+                onPressed: () => runDartFrogServer(),
+                child: const Text('Start Server with Dart Frog Web Server'),
+              ),
+              ElevatedButton(
+                onPressed: () => runShelfServer(),
+                child: const Text('Start Server with Shelf Web Server'),
+              ),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+void runDartFrogServer() async {
+  // This assumes the production dart_frog server was generated in `build`.
+  // To generate the production server run: "dart_frog build" from within
+  // the packages/web_server directory.
+  final process = await Process.start('dart', [
+    p.join('packages', 'web_server', 'build', 'bin', 'server.dart'),
+  ]);
+  await process.exitCode;
 }
